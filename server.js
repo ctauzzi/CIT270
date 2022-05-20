ca58308d124f8fe69de1ffa04890dae3210362e7
Cayson Fantauzzi <caysonfantauzzi@gmail.com>
2022-05-20 13:41:13 -0600

    files updated

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require('express'); //import the library
 const bodyParser = require('body-parser'); //body parse is called middleware
 const port = 3000;
-const app = express();//use the library
 const md5 = require('md5');
-const redis = require('redis');
+const app = express();//use the library
+const {createClient} = require('redis');
+const redisClient = createClient(
+    {
+        socket:{
+            port:6379,
+            host:"127.0.0.1",
+        },
+    }
+);
 
-const redisClient = redis.createClient();
+app.use(bodyParser.json()); //use the middleware (calls it before anything else happens on each request)
 
-app.use(bodyParser.json());//use the middleware (call it before anything else happens on each request)
-
-app.listen(port, ()=>{
+app.listen(port, async()=>{
+    await redisClient.connect();
     console.log("Listening on port: " + port)
-});//listen
+});
 
-app.post('/login', async(request, response)=>{//a post is when a client sends new information to an API
+const validatePassword = async(request, response)=>{
     const requestHashedPassword = md5(request.body.password);
-    const redisHashedPassword = redisClient.hGet('passwords', request.body.userName);
-    const loginRequest = request.body;
-    console.log("Request body", JSON.stringify(request.body)); //just to see what is contained in the body of the json file
-    //search database for username, and retrieve current password
-    
-
+    const redisHashedPassword = await redisClient.hmGet('passwords', request.body.userName); //use await here, if await is not used this line of code is only a promise
 
     //compare the hashed version of the password that was sent with the hashed version from the database
-    if (loginRequest.userName == "notrealemail@gmail.com" && loginRequest.password == "Passw0rd!"){
+    if (requestHashedPassword == redisHashedPassword){
         response.status(200);//200 means OK
         response.send("Welcome");
     }
@@ -31,8 +33,17 @@ app.post('/login', async(request, response)=>{//a post is when a client sends ne
         response.status(401);//401 means unauthorized
         response.send("Unauthorized");
     }
-})
+};
+
+const signup = (request, response)=>{
+    await redisClient.connect();
+    const requestUserName = (request.body.userName);
+    const requestPassword = md5(request.body.password);
+    const redisSignup = await redisClient.hSet('passwords', request.body.userName, request.body.password);
+};
 
 app.get('/', (request, response)=>{ //everytime something calls your API that is a request
     response.send("Hello")//a response is when the API gives the information requested
 });
+
+app.post('/login', validatePassword);
