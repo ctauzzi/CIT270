62a1b6c19465e822a96b91a528daf4d3e9bd277b
ctauzzi <caysonfantauzzi@gmail.com>
2022-06-21 13:43:29 -0600

    files updated

--- apiclient.bat
+++ apiclient.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 curl -d "@data.json" -X POST -H "Content-Type:application/json" https://cayson.cit270.com:3000/login
-=======
-curl --insecure -d "@data.json" -X POST -H "Content-Type:application/json" https://localhost:3000/login
->>>>>>> f436fa23231d570dac22d1522a6fb7520254307d
