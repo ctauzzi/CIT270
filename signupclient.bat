1e76c58a2c411ecf092e0f81f12989e43d135d83
ctauzzi <104652191+ctauzzi@users.noreply.github.com>
2022-07-14 11:34:25 -0600

    Update signupclient.bat

--- signupclient.bat
+++ signupclient.bat
@@ -1,1 +1,1 @@
-curl -d "@signup.json" -X POST -H "Content-Type:application/json" https://cayson-nodejs.cit270.com/signup
+curl -d "@signup.json" -X POST -H "Content-Type:application/json" https://caysonfantauzzi.cit270.com/signup
